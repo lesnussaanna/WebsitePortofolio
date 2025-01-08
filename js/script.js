a51dce449627c118449b02fbd6ffd2dfbcdb18d2
lesnussaanaa <89694001+lesnussaanna@users.noreply.github.com>
2025-01-08 12:56:38 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const nav = document.getElementById('nav');
+
+window.addEventListener('scroll',function()
+{
+    scrollposition =this.window.scrollY;
+
+    if( scrollposition >=60)
+    {
+        nav.classList.add('nav-dark')
+    }
+    else if (scrollposition <=60){
+        nav.classList.remove('nav-dark');
+    }
+})
